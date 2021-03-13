4843f1451b863580463c5fd1da604ff20e6a26b6
JAYANTSINGHSIDDHU <80543952+JAYANTSINGHSIDDHU@users.noreply.github.com>
2021-03-13 19:07:01 +0530

    free hosting

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Personal_Blog.Global" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Personal_Blog.Startup))]
+namespace Personal_Blog
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
